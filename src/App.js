f0ae7dbc72c0b8962a8ac5d6627f251ab49b6fa8
Pavi Khanna <princesspavikhanna@gmail.com>
2020-12-30 18:07:29 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FirstComponent from './components/FirstComponent';
+import './App.css';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import NavigationBar from './components/NavigationBar';
+import axios from 'axios';
+
+class App extends Component{
+
+  state = {
+    isLoading: false,
+    products: []
+  };
+
+
+componentDidMount(){
+  fetch('http://localhost:8081/getAll')
+        .then(response => {return response.json();})
+        .then(json => {
+          this.setState(
+            {
+              isLoading:true,
+              products: json,
+            }
+          )
+        })
+}
+
+  render(){
+
+    const {products,isLoading} = this.state;
+
+    if(!isLoading){
+      return <div>Loading...</div>
+    }
+    else {
+      return (
+        <table className='table'>
+          <thead>
+          <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Available</th>
+          <th>Price</th>
+          <th>Categories</th>
+          </tr>
+          </thead>
+          <tbody>
+        {
+          products.map(product=>(
+            <tr key={product.id}>
+              <td className='id'>{ product.id }</td>
+              <td>{ product.name }</td>
+              <td className='center'>{ product.available?"YES":"NO" }</td>
+              <td>{ product.price }</td>
+              <td>{ product.category.reduce((acc,catg)=>acc+catg+' ',"") }</td>
+            </tr>
+          ))
+        }
+          </tbody>
+        </table>
+      )
+    }
+  }
+}
+
+class SecondComponent extends Component{
+  render(){
+    return (
+      <div className="secondComponent">
+        Second Component
+      </div>
+    );
+  }
 }
 
 export default App;
